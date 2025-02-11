0d56f79938efb79c0a4efce02ffd54b57f4efd51
shenkzjay <senksjay@gmail.com>
2025-02-11 18:03:13 +0100

    fixed deployment issues

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,3 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [
-  index("routes/home.tsx"),
-  route("rag", "./routes/rag.tsx"),
-  route("lang", "./routes/langgraph.tsx"),
-] satisfies RouteConfig;
+export default [index("routes/rag.tsx"), route("rag", "./routes/home.tsx")] satisfies RouteConfig;
